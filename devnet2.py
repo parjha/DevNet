96dfdae60229e2499ab658f602bece69cfff242e
Paridhi Jha (parjha) <parjha@cisco.com>
2021-02-02 20:15:50 +0530

    adding second file

---
+++ devnet2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  2 18:56:39 2021
+
+@author: parjha
+"""
+
+a=input("Enter your name:")
+c=input("Enter your college:")
+b=1
+while b<=5:
+    print(a,c) 
+    b=b+1
